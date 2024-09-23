ef6923d2ae842aa40843164c80706446a03bfc28
[ilin117] <[ilin1172004@gmail.com]>
2024-09-23 12:39:32 -0400

    updated

--- calculator.py
+++ calculator.py
@@ -1,25 +1,24 @@
 # integral calculator using sympy expressions
 from PyDesmos import Graph, to_latex
-from sympy import sympify
+from sympy import sympify, SympifyError
+import webbrowser
+import os
 
 function = input("Enter a function: ")
 boundStart = int(input("Enter your starting bound: "))
 boundEnd = int(input("Enter where your bound ends: "))
 
-# constants throws an error when parsed, so we'll separate it
-constant = ""
-i = 0
-while i < len(function) - 1 and function[i].isdigit():
-    constant += function[i]
-    i += 1
 
-reduced_function = function[i:]
+try:
+    with Graph('my_graph') as G:
+        G("f(x) = " + to_latex(sympify(function)))
+        G("\\int_{a}^{b}" + "f(x)dx")  # integral
+        G(a=boundStart)  # variable a at boundStart
+        G(b=boundEnd)  # variable b at boundEnd
+        # shades in bounded region
+        G(r"0\le y\le f\left(x\right)\left\{a<x<b\right\}")
+        G(r"0\ge y\ge f\left(x\right)\left\{a<x<b\right\}")
+except SympifyError:
+    print(f'Function in incorrect format. ex: (6/x^3) - (2*x)')
 
-with Graph('my_graph') as G:
-    G("f(x) = " + constant + to_latex(sympify(reduced_function)))
-    G("\\int_{a}^{b}" + "f(x)dx")  # integral
-    G(a=boundStart)  # variable a at boundStart
-    G(b=boundEnd)  # variable b at boundEnd
-    # shades in bounded region
-    G(r"0\le y\le f\left(x\right)\left\{a<x<b\right\}")
-    G(r"0\ge y\ge f\left(x\right)\left\{a<x<b\right\}")
+webbrowser.open(f'file://{os.path.realpath('my_graph.html')}')
